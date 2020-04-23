d5124a7eb40fd1d698f23996ffd07b1286f63ad3
freelook.info <freelook@mail.ua>
2020-04-23 14:37:50 +0000

    fix locale on index

--- components/link.tsx
+++ components/link.tsx
@@ -4,11 +4,11 @@ import Link, {LinkProps as LP} from 'next/link';
 export type LinkProps = LP;
 
 export default (props:LinkProps&any) => {
-    const as = props.as || props.href;
+    const as = props.as || props.href || '';
     const query = as.split('?')[1];
     const prefetch = false;
-    let href = props.href;
-    if(href !== '/') {
+    let href = props.href || '';
+    if(href.split('?')[0] !== '/') {
       href = '/[...path]' + (query ? `?${query}` : '');
     }
     return <Link {...{as, href, prefetch}}>{props.children}</Link>;
